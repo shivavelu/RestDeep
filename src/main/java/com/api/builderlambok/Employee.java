b7a8a02c80eec5be61788905839e7a19181aea5d
siva <shivavelu@gmail.com>
2024-01-15 05:59:17 +0530

    modity gitignore

--- src/main/java/com/api/builderlambok/Employee.java
+++ src/main/java/com/api/builderlambok/Employee.java
@@ -1,5 +1,7 @@
 package com.api.builderlambok;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreType;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
 import java.util.List;
@@ -8,6 +10,8 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @ToString
+@JsonInclude(value= JsonInclude.Include.NON_EMPTY) // this ignore the attribute if the value is null & empty and Inclue.NON_NULL ignore only NULL value
+// NON_EMPTY ignore both null and empty attribute
 @Builder // should have builder annotation only as builder() travel from here
 public class Employee {
 
