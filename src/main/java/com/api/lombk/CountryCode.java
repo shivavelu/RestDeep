f19d0f33a5c1b6a124a4444e06aebca6c33bab41
siva <shivavelu@gmail.com>
2024-01-16 13:04:16 +0530

    added array response validation

---
+++ src/main/java/com/api/lombk/CountryCode.java
@@ -0,0 +1,18 @@
+package com.api.lombk;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+@Builder
+public class CountryCode {
+    private int bookingid;
+
+    public CountryCode() {
+
+    }
+
+
+}
